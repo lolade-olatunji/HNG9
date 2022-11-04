baca061f5e206c5d19e78286d1f58d284c53037a
lolade-olatunji <ololade0507@gmail.com>
2022-11-04 11:12:13 +0100

    first commit on contact

--- src/App.js
+++ src/App.js
@@ -1,80 +1,17 @@
-import React, { useDebugValue } from 'react';
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import 'primeflex/primeflex.css';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home/home';
+import Contact from './pages/contact/contact';
 
-import { Avatar } from 'primereact/avatar';
-import { Button } from 'primereact/button';
-import Footer from './components/Footer';
-import back1 from './assets/images/_Avatar share button (1).png';
-import back from './assets/images/_Avatar share button.png';
-
-function App() {
-	const data = {
-		name: 'Annett Black',
-		image: 'http://github.com/lolade-olatunji.png',
-		links: [
-			{
-				name: 'Twitter Link',
-				link: 'http://twitter.com/lolade',
-				id: 'twitter'
-			},
-			{
-				name: 'Zuri Team',
-				link: 'https://training.zuri.team/',
-				id: 'btn__zuri'
-			},
-			{
-				name: 'Zuri Books',
-				link: ' http://books.zuri.team',
-				id: 'books'
-			},
-			{
-				name: 'Python Books',
-				link: 'https://books.zuri.team',
-				id: 'book__python'
-			},
-			{
-				name: 'Background Check for Coders',
-				link: 'https://background.zuri.team',
-				id: 'pitch'
-			},
-			{
-				name: 'Design Books',
-				link: 'https://books.zuri.team/design-rules',
-				id: 'book_design'
-			}
-		]
-	};
-
+const App = () => {
 	return (
-		<div className="p-m-4">
-			<div className="content-wrap">
-				<div className="p-d-flex p-jc-center p-ai-center p-flex-column">
-					<Avatar id="profile__img" image={data.image} size="xlarge" shape="circle" />
-					<h1 className="p-m-2">{data.name}</h1>
-				</div>
-
-				<div className="p-d-flex p-jc-center p-ai-center">
-					<div className="button">
-						{data.links.map(({ link, name, id}) => (
-							<a id={id} className="p-m-2 p-p-3" href={link}>
-							{name}
-							</a>
-						))}
-					</div>
-				</div>
-
-				<div className="back">
-					<img className="back1" src={back} alt="back" />
-					<img className="back2" src={back1} alt="back" />
-				</div>
-			</div>
-
-			<Footer />
+		<div className="app">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/contact" element={<Contact />} />
+			</Routes>
 		</div>
 	);
-}
+};
 
 export default App;
---
+++ src/pages/contact/contact.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import './contact.css';
+import Footer from '../../components/Footer';
+
+const contact = () => {
+	return (
+		<div id="contact-page">
+
+			<div className="container">
+
+				<div className="header">
+					<h1 className="co-me">Contact Me</h1>
+					<p className="sub">Hi there, contact me to ask me about anything you have in mind.</p>
+				</div>
+
+            <div className='form-section'>
+
+            <div className="form-inline">
+              <div className="form-control first">
+                <label className="form-label" for="first-name">First Name</label>
+                <input className="form-input" type="text" id="first-name"  placeholder="Enter your first name"/>
+              </div>
+
+              <div className="form-control last">
+                <label className="form-label" for="Last-Name">Last Name</label>
+                <input className="form-input" type="text" id="Last-Name"  placeholder="Enter your last name" />
+              </div>
+            </div>
+
+            <div className="form-control">
+              <label className="form-label" for="email">Email</label>
+              <input className="form-input" type="email" id="email"  placeholder="yourname@email.com" />
+            </div>
+
+            <div className="form-control">
+            <label className="form-label" for="Meesage">Message</label>
+            <textarea name="Message" rows="6" placeholder="Send me a message and I'll reply you as soon as possible"></textarea>
+            </div>
+            </div>
+			</div>
+      <Footer />
+		</div>
+
+	);
+};
+
+export default contact;
